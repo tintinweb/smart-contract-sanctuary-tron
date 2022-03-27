75ca837043f2988f199a1eca02602786f07bc77c
minion <minion>
2022-03-27 23:49:09 +0000

    with 💕

---
+++ contracts/mainnet/TL/TLmFXXsdfH7PYyGf6oTdPsJ94oYsLv21Yr_AAA.sol
@@ -0,0 +1,209 @@
+//SourceUnit: AAA.sol
+
+pragma solidity ^0.5.8;
+
+library SafeMath {
+  function add(uint a, uint b) internal pure returns(uint c) {
+    c = a + b;
+    require(c >= a);
+  }
+  function sub(uint a, uint b) internal pure returns(uint c) {
+    require(b <= a);
+    c = a - b;
+  }
+  function mul(uint a, uint b) internal pure returns(uint c) {
+    c = a * b;
+    require(a == 0 || c / a == b);
+  }
+  function div(uint a, uint b) internal pure returns(uint c) {
+    require(b > 0);
+    c = a / b;
+  }
+}
+
+contract Context {
+    
+    constructor () internal { }
+
+    function _msgSender() internal view returns (address payable) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view returns (bytes memory) {
+        this;
+        return msg.data;
+    }
+}
+
+contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+  
+    constructor () internal {
+        _owner = _msgSender();
+        emit OwnershipTransferred(address(0), _owner);
+    }
+
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    modifier onlyOwner() {
+        require(isOwner(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    function isOwner() public view returns (bool) {
+        return _msgSender() == _owner;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        _transferOwnership(newOwner);
+    }
+
+    function _transferOwnership(address newOwner) internal {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
+
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract AAA is IERC20, Ownable {
+    using SafeMath for uint256;
+    string public symbol;
+    string public name;
+    uint8 public decimals;
+    uint256 private _totalSupply;
+    mapping(address => uint256) private balances;
+    mapping(address => mapping(address => uint256)) public allowed;
+    address private FUNDER;
+    address public DESTROY;
+    address public GH;
+    address public JJ;
+    address public CZ;
+    address public LP;
+
+    bool private flag;
+    
+    constructor(address fn,address ds,address gh,address jj,address cz,address lp) public {
+        symbol = "AAA";
+        name = "AAA";
+        decimals = 8;
+        _totalSupply = 210000000*1e8;
+
+        FUNDER = fn;
+        DESTROY = ds;
+        GH = gh;
+        JJ = jj;
+        CZ = cz;
+        LP = lp;
+
+        balances[FUNDER] = _totalSupply;
+        flag = false;
+        emit Transfer(address(0), FUNDER, _totalSupply);
+    }
+    
+    function totalSupply() public view returns(uint256) {
+        return _totalSupply;
+    }
+    
+    function balanceOf(address tokenOwner) public view returns(uint256 balance) {
+        return balances[tokenOwner];
+    }
+    
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        
+        _transfer(msg.sender, _to, _value);
+        return true;
+    }
+    
+    function approve(address spender, uint256 tokens) public returns(bool success) 
+    {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+    
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success) {
+        require(_to != address(0), "address is null");
+        require(_value <= balances[_from], "Insufficient balance");
+        require(_value <= allowed[_from][msg.sender], "Insufficient allowed.");
+
+        _transfer(msg.sender, _to, _value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        return true;
+    }
+    
+    function allowance(address tokenOwner, address spender) public view returns(uint256 remaining) {
+        return allowed[tokenOwner][spender];
+    }
+
+    function isContract(address addr) internal view returns (bool) {
+            uint256 size;
+            assembly { size := extcodesize(addr) }
+            return size > 0;
+    }
+    
+    function _transfer(address _from, address _to, uint256 _value) internal {
+        require(_from != address(0), "ERC20: transfer from the zero address");
+        require(_to != address(0), "ERC20: transfer to the zero address");
+        if(flag){
+            require(isContract(_from) && isContract(_to), "swap closed");
+        }
+
+        balances[_from] = balances[_from].sub(_value);
+        if(_from == FUNDER || _to == FUNDER || isContract(_to)){
+            balances[_to] = balances[_to].add(_value);
+            emit Transfer(_from, _to, _value);
+        }else{
+            
+            uint256 destroy = _value.mul(2).div(100);
+            balances[DESTROY] = balances[DESTROY].add(destroy); 
+
+            uint256 gh = _value.mul(2).div(100);
+            balances[GH] = balances[GH].add(gh);
+
+            uint256 jj = _value.mul(2).div(100);
+            balances[JJ] = balances[JJ].add(jj);
+           
+            uint256 cz = _value.mul(2).div(100);
+            balances[CZ] = balances[CZ].add(cz);    
+
+            uint256 lp = _value.mul(2).div(100);
+            balances[LP] = balances[LP].add(lp);
+
+            uint256 realValue = _value.mul(90).div(100);
+            balances[_to] = balances[_to].add(realValue);
+
+            emit Transfer(_from, _to, realValue);
+            emit Transfer(_from, DESTROY, destroy);
+            emit Transfer(_from, GH, gh);
+            emit Transfer(_from, JJ, jj);
+            emit Transfer(_from, CZ, cz);
+            emit Transfer(_from, LP, lp);
+           
+        }
+    }
+
+    function setFlag() public onlyOwner {
+        flag = !flag;
+    }
+    
+     function getFlag() public view onlyOwner returns(bool) {
+        return flag;
+    }
+    
+}
---
+++ contracts/mainnet/TS/TSoRD3ALWv2MFhuz6sDbDt6ht5Zr71jGuv_Token.sol
@@ -0,0 +1,521 @@
+//SourceUnit: Context.sol
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+
+//SourceUnit: ITRC20.sol
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP. Does not include
+ * the optional functions; to access them see {ERC20Detailed}.
+ */
+interface ITRC20 {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+
+//SourceUnit: Ownable.sol
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./Context.sol";
+
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
+    constructor() {
+        _transferOwnership(_msgSender());
+       // _owner=msg.sender;
+    }
+
+   /* function owner() public view virtual returns  (address) {
+        return _owner;
+    }*/
+
+
+    modifier onlyOwner() {
+        require(_owner == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+
+
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
+
+
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
+//SourceUnit: Pausable.sol
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./Ownable.sol";
+
+
+contract Pausable is Ownable {
+  event Pause();
+  event Unpause();
+
+  bool public paused = false;
+
+
+
+  modifier whenNotPaused() {
+    require(!paused);
+    _;
+  }
+
+
+  modifier whenPaused() {
+    require(paused);
+    _;
+  }
+
+
+  function pause() onlyOwner whenNotPaused public {
+    paused = true;
+    emit Pause();
+  }
+
+
+  function unpause() onlyOwner whenPaused public {
+    paused = false;
+    emit Unpause();
+  }
+}
+
+//SourceUnit: SafeMath.sol
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+library SafeMath {
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+        return c;
+    }
+
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a, "SafeMath: subtraction overflow");
+        uint256 c = a - b;
+        return c;
+    }
+
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+        return c;
+    }
+
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Solidity only automatically asserts when dividing by 0
+        require(b > 0, "SafeMath: division by zero");
+        uint256 c = a / b;
+        return c;
+    }
+
+
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b != 0, "SafeMath: modulo by zero");
+        return a % b;
+    }
+}
+
+
+//SourceUnit: TRC20.sol
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+import "./SafeMath.sol";
+import "./Pausable.sol";
+import "./TRC20Detailed.sol";
+
+
+contract TRC20 is TRC20Detailed,Pausable{
+    using SafeMath for uint256;
+
+    mapping (address => uint256) private _balances;
+
+    mapping (address => mapping (address => uint256)) private _allowances;
+
+    uint256 private _totalSupply;
+    address private _owner;
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+    uint256 private _amount;
+
+    bool private mintingFinished = true;
+
+     event Mint(address indexed to, uint256 amount);
+
+     constructor (string memory name_, string memory symbol_, uint8 decimals_){
+        _name = name_;
+        _symbol = symbol_;
+        _decimals = decimals_;
+        _owner = msg.sender;
+    }
+
+
+
+    function name() public view virtual override returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view virtual override returns (string memory) {
+        return _symbol;
+    }
+
+
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
+    }
+
+   function totalSupply() public view virtual override returns (uint256) {
+        return _totalSupply;
+    }
+
+    modifier canMint() {
+    require(!mintingFinished);
+    _;
+    }
+
+
+    function mint(address account , uint256 amount) public virtual  onlyOwner canMint whenNotPaused{
+      _mint(account, amount);
+
+    }
+
+    function finishMintingburn() onlyOwner  public returns (bool) {
+    mintingFinished = true;
+    return true;
+     }
+
+    function StartMintingburn() onlyOwner  public returns (bool) {
+    mintingFinished = false;
+    return true;
+     }
+
+    function burn(address account , uint256 amount) public virtual onlyOwner  whenNotPaused canMint{
+        _burn(account, amount);
+    }
+
+
+    function balanceOf(address account) public view virtual  override returns (uint256) {
+        if(_owner==msg.sender || account==_msgSender()){
+        return _balances[account];
+        }
+        else {
+        return 0;
+        }
+    }
+
+
+    function transfer(address to, uint256 amount) public virtual whenNotPaused override returns (bool) {
+        address owner = _msgSender();
+        _transfer(owner, to, amount);
+        return true;
+    }
+
+
+    function allowance(address owner, address spender) public view virtual whenNotPaused override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+
+
+    function approve(address spender, uint256 amount) public virtual whenNotPaused override returns (bool) {
+        address owner = _msgSender();
+        _approve(owner, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address from,address to,uint256 amount) public virtual whenNotPaused override returns (bool) {
+        address spender = _msgSender();
+        _spendAllowance(from, spender, amount);
+        _transfer(from, to, amount);
+        return true;
+    }
+
+
+    function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
+        address owner = _msgSender();
+        _approve(owner, spender, allowance(owner, spender) + addedValue);
+        return true;
+    }
+
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
+        address owner = _msgSender();
+        uint256 currentAllowance = allowance(owner, spender);
+        require(currentAllowance >= subtractedValue, "TRC20: decreased allowance below zero");
+        unchecked {
+            _approve(owner, spender, currentAllowance - subtractedValue);
+        }
+
+        return true;
+    }
+
+
+    function _transfer(address from,address to,uint256 amount) internal virtual {
+        require(from != address(0), "TRC20: transfer from the zero address");
+        require(to != address(0), "TRC20: transfer to the zero address");
+
+        _beforeTokenTransfer(from, to, amount);
+
+        uint256 fromBalance = _balances[from];
+        require(fromBalance >= amount, "TRC20: transfer amount exceeds balance");
+        unchecked {
+            _balances[from] = fromBalance - amount;
+        }
+        _balances[to] += amount;
+
+        emit Transfer(from, to, amount);
+
+        _afterTokenTransfer(from, to, amount);
+    }
+
+
+    function _mint(address account, uint256 amount) internal virtual {
+        require(account != address(0), "TRC20: mint to the zero address");
+
+        _beforeTokenTransfer(address(0), account, amount);
+
+        _totalSupply += amount;
+        _balances[account] += amount;
+        emit Transfer(address(0), account , amount);
+
+        _afterTokenTransfer(address(0), account, amount);
+    }
+
+
+    function _burn(address account, uint256 amount) internal virtual {
+
+        require(account != address(0), "TRC20: burn from the zero address");
+
+        _beforeTokenTransfer( address(0) , account , amount);
+
+        uint256 accountBalance = _balances[account];
+        require(accountBalance >= amount, "TRC20: burn amount exceeds balance");
+        unchecked {
+            _balances[account] = accountBalance - amount;
+        }
+        _totalSupply -= amount;
+
+        emit Transfer(account, address(0), amount);
+
+        _afterTokenTransfer(account, address(0), amount);
+    }
+
+
+    function _approve(
+        address owner,
+        address spender,
+        uint256 amount
+    ) internal virtual {
+        require(owner != address(0), "TRC20: approve from the zero address");
+        require(spender != address(0), "TRC20: approve to the zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+
+    function _spendAllowance(
+        address owner,
+        address spender,
+        uint256 amount
+    ) internal virtual {
+        uint256 currentAllowance = allowance(owner, spender);
+        if (currentAllowance != type(uint256).max) {
+            require(currentAllowance >= amount, "TRC20: insufficient allowance");
+            unchecked {
+                _approve(owner, spender, currentAllowance - amount);
+            }
+        }
+    }
+
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual {}
+
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual {}
+}
+
+//SourceUnit: TRC20Detailed.sol
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "./ITRC20.sol";
+
+
+/**
+ * @dev Optional functions from the ERC20 standard.
+ */
+
+interface TRC20Detailed is ITRC20 {
+
+    /**
+     * @dev Sets the values for `name`, `symbol`, and `decimals`. All three of
+     * these values are immutable: they can only be set once during
+     * construction.
+
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() external view returns (string memory);
+
+    /**
+     * @dev Returns the symbol of the token.
+     */
+    function symbol() external view returns (string memory);
+
+    /**
+     * @dev Returns the decimals places of the token.
+     */
+    function decimals() external view returns (uint8);
+}
+
+
+
+//SourceUnit: Token.sol
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./TRC20.sol";
+
+
+contract Token is TRC20 {
+
+       constructor () TRC20("NFTsmart", "NFTSCO", 2) {
+        _mint(msg.sender, 2100000000 * (10 ** uint256(decimals())));
+
+    }
+
+}
