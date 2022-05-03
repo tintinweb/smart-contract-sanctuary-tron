720ff93a4cf986c0d22bd739282d96a53680663b
minion <minion>
2022-05-03 23:49:20 +0000

    with 💕

---
+++ contracts/mainnet/TQ/TQD9CFAvcj8x4pmXKH9LVv86LLsxG8o8ST_Token.sol
@@ -0,0 +1,282 @@
+//SourceUnit: ERC20.sol
+
+pragma solidity ^0.5.0;
+
+import "./IERC20.sol";
+import "./SafeMath.sol";
+
+contract ERC20 is IERC20 {
+    using SafeMath for uint256;
+
+    mapping (address => uint256) private _balances;
+
+    mapping (address => mapping (address => uint256)) private _allowances;
+
+    uint256 private _totalSupply;
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address recipient, uint256 amount) public returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender) public view returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 value) public returns (bool) {
+        _approve(msg.sender, spender, value);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
+        _transfer(sender, recipient, amount);
+        _approve(sender, msg.sender, _allowances[sender][msg.sender].sub(amount));
+        return true;
+    }
+
+    function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
+        _approve(msg.sender, spender, _allowances[msg.sender][spender].add(addedValue));
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
+        _approve(msg.sender, spender, _allowances[msg.sender][spender].sub(subtractedValue));
+        return true;
+    }
+
+    function _transfer(address sender, address recipient, uint256 amount) internal {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+
+        _balances[sender] = _balances[sender].sub(amount);
+        _balances[recipient] = _balances[recipient].add(amount);
+        emit Transfer(sender, recipient, amount);
+    }
+
+    function _mint(address account, uint256 amount) internal {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        _totalSupply = _totalSupply.add(amount);
+        _balances[account] = _balances[account].add(amount);
+        emit Transfer(address(0), account, amount);
+    }
+
+    function _burn(address account, uint256 value) internal {
+        require(account != address(0), "ERC20: burn from the zero address");
+
+        _totalSupply = _totalSupply.sub(value);
+        _balances[account] = _balances[account].sub(value);
+        emit Transfer(account, address(0), value);
+    }
+
+    function _approve(address owner, address spender, uint256 value) internal {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[owner][spender] = value;
+        emit Approval(owner, spender, value);
+    }
+
+    function _burnFrom(address account, uint256 amount) internal {
+        _burn(account, amount);
+        _approve(account, msg.sender, _allowances[account][msg.sender].sub(amount));
+    }
+}
+
+//SourceUnit: ERC20Detailed.sol
+
+pragma solidity ^0.5.0;
+
+import "./IERC20.sol";
+
+contract ERC20Detailed is IERC20 {
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+
+    constructor (string memory name, string memory symbol, uint8 decimals) public {
+        _name = name;
+        _symbol = symbol;
+        _decimals = decimals;
+    }
+
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public view returns (uint8) {
+        return _decimals;
+    }
+}
+
+
+//SourceUnit: IERC20.sol
+
+pragma solidity ^0.5.0;
+
+interface IERC20 {
+
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+	
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+//SourceUnit: Ownable.sol
+
+pragma solidity 0.5.10;
+
+contract Ownable {
+
+    address public owner;
+   
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, 'erronlyOwnererr');
+        _;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        if (newOwner != address(0)) {
+            owner = newOwner;
+        }
+    }
+	
+}
+
+//SourceUnit: SafeMath.sol
+
+pragma solidity ^0.5.0;
+
+library SafeMath {
+
+	//加
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+        return c;
+    }
+	
+	//减
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a, "SafeMath: subtraction overflow");
+        uint256 c = a - b;
+        return c;
+    }
+	
+	//乘
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+        return c;
+    }
+	
+	//除
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0, "SafeMath: division by zero");
+        uint256 c = a / b;
+        return c;
+    }
+	
+	//余
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b != 0, "SafeMath: modulo by zero");
+        return a % b;
+    }
+}
+
+//SourceUnit: Token.sol
+
+pragma solidity ^0.5.0;
+
+import "./ERC20.sol";
+import "./ERC20Detailed.sol";
+import "./Ownable.sol";
+import "./SafeMath.sol";
+
+contract Token is ERC20, ERC20Detailed, Ownable {
+    using SafeMath for uint256;
+
+    address public pairAddress = 0x0000000000000000000000000000000000000000;
+
+    address public destroyAddress = 0x0000000000000000000000000000000000000001;
+
+    mapping(address => bool) public whiteList;
+
+    uint public feePercent = 1000;
+    uint public transFees;
+    uint public sellFees;
+    address public feeTo;
+
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint _total) public ERC20Detailed(_name, _symbol, _decimals) {
+        _mint(msg.sender, _total * (10 ** uint256(decimals())));
+        feeTo = owner;
+    }
+
+    function setFeeTo(address _addr) public onlyOwner {
+        feeTo = _addr;
+    }
+
+    function transFeesin(uint _fee,uint _sellfee) public onlyOwner {
+        transFees = _fee;
+        sellFees = _sellfee;
+    }
+
+    function setPairAddress(address _pairAddress) public onlyOwner {
+        pairAddress = _pairAddress;
+    }
+
+    function setWhiteList(address _addr, bool _v) public onlyOwner {
+        whiteList[_addr] = _v;
+    }
+
+    function _transfer(address sender, address recipient, uint256 amount) internal {
+        if(!whiteList[sender] && !whiteList[recipient]) {
+            uint fees = 0;
+            if (recipient == pairAddress) {
+                fees = amount.mul(sellFees).div(feePercent);
+                if(fees>0){
+                    super._transfer(sender, feeTo, fees);
+                }
+                amount = amount.sub(fees);
+            }else if(sender != pairAddress) {
+                fees = amount.mul(transFees).div(feePercent);
+                if(fees>0){
+                    super._transfer(sender, feeTo, fees);
+                }
+                amount = amount.sub(fees);
+            }
+        }
+
+        super._transfer(sender, recipient, amount);
+    }
+}
