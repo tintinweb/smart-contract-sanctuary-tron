f0f517a83f91e22fb47c141c7a939a8866cfe355
minion <minion>
2022-05-23 23:51:22 +0000

    with 💕

---
+++ contracts/mainnet/TX/TXRd7zAkSGExxj3eXTiAbwiHQUCSW1DMhJ_transferTokenContract.sol
@@ -0,0 +1,24 @@
+//SourceUnit: trc10.sol
+
+pragma solidity ^0.5.0;
+
+contract transferTokenContract {
+    constructor() payable public{}
+    
+    function() payable external {}
+    
+    function transferTokenTest(address payable toAddress, uint256 tokenValue, trcToken id) payable public    {
+        toAddress.transferToken(tokenValue, id);
+    }
+    
+    function msgTokenValueAndTokenIdTest() public payable returns(trcToken, uint256){
+        trcToken id = msg.tokenid;
+        uint256 value = msg.tokenvalue;
+        return (id, value);
+    }
+    
+    function getTokenBalanceTest(address accountAddress) payable public returns (uint256){
+        trcToken id = 1000001;
+        return accountAddress.tokenBalance(id);
+    }
+}
