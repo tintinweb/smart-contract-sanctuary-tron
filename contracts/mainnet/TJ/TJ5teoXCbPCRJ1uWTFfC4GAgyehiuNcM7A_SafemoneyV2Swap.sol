a895412d96f44f34c0515d45808621903af09e64
minion <minion>
2022-06-15 23:52:21 +0000

    with 💕

---
+++ contracts/mainnet/TJ/TJ5teoXCbPCRJ1uWTFfC4GAgyehiuNcM7A_SafemoneyV2Swap.sol
@@ -0,0 +1,207 @@
+//SourceUnit: SafemoneyV2Swap.sol
+
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.6;
+import "./context.sol";
+interface TRCTOKEN {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the token decimals.
+     */
+    function decimals() external view returns (uint8);
+
+    /**
+     * @dev Returns the token symbol.
+     */
+    function symbol() external view returns (string memory);
+
+    /**
+     * @dev Returns the token name.
+     */
+    function name() external view returns (string memory);
+
+    /**
+     * @dev Returns the bep token owner.
+     */
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address _owner, address spender)
+        external
+        view
+        returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
+
+
+contract SafemoneyV2Swap is Ownable{
+
+
+    address public v1TokenAddress=0x8605caE8c40545D2184D59550918185f44D9ea0d;
+    address public v2TokenAddress=0xAAB8c5F22e5e489a5824D8653F9BA68eb85FDE04;
+    TRCTOKEN public v1SafeMoney=TRCTOKEN(v1TokenAddress);
+    TRCTOKEN public v2SafeMoney=TRCTOKEN(v2TokenAddress);
+
+    address public burnAddress=0x77944D19C052B73Ee2286823AA83F8138cb7032f;
+
+    function setv1SateMoney(address _v1TokenAddress) public onlyOwner{
+        v1SafeMoney=TRCTOKEN(_v1TokenAddress);
+    }
+    function setv2SafeMoney(address _v2TokenAddress)public onlyOwner{
+        v2SafeMoney=TRCTOKEN(_v2TokenAddress);
+    }
+
+    function setburnAddress(address _add) public onlyOwner{
+        burnAddress=_add;
+    }
+
+
+    function swapExactV1ForV2(uint256 v1Amount) public {
+        v1SafeMoney.transferFrom(msg.sender,address(this),v1Amount);
+        v1SafeMoney.transfer(burnAddress,v1Amount);
+        v2SafeMoney.transfer(msg.sender,v1Amount);
+    }
+
+
+
+}
+
+
+//SourceUnit: context.sol
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        return msg.data;
+    }
+}
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor () {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+    function transferOwnerShip(address newOwner) public onlyOwner {
+        _owner=newOwner;
+        emit OwnershipTransferred(address(0), newOwner);
+    }
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    function transferOwnership(address transferTo) public onlyOwner{
+        emit OwnershipTransferred(_owner, transferTo);
+        _owner = transferTo;
+    }
+}
