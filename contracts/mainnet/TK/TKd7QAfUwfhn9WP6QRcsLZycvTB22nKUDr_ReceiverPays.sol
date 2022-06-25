1e5d2552a6a4c992b6c536f1ebed121cbb146b5b
minion <minion>
2022-06-25 17:04:22 +0000

    with 💕

---
+++ contracts/mainnet/TK/TKd7QAfUwfhn9WP6QRcsLZycvTB22nKUDr_ReceiverPays.sol
@@ -0,0 +1,122 @@
+//SourceUnit: tronreceiverpay.sol
+
+pragma solidity >=0.4.22;
+
+contract TokenInterface {
+    /* This is a slight change to the ERC20 base standard.
+    function totalSupply() constant returns (uint256 supply);
+    is replaced with:
+    uint256 public totalSupply;
+    This automatically creates a getter function for the totalSupply.
+    This is moved to the base contract since public getter functions are not
+    currently recognised as an implementation of the matching abstract
+    function by the compiler.
+    */
+    /// total amount of tokens
+    uint256 public totalSupply;
+
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
+    function balanceOf(address _owner) public view returns (uint256 balance);
+
+    /// @notice send `_value` token to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transfer(address _to, uint256 _value) public returns (bool success);
+
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+
+    /// @notice `msg.sender` approves `_spender` to spend `_value` tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _value The amount of tokens to be approved for transfer
+    /// @return Whether the approval was successful or not
+    function approve(address _spender, uint256 _value) public returns (bool success);
+
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return Amount of remaining tokens allowed to spent
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining);
+
+    // solhint-disable-next-line no-simple-event-func-name
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+contract ReceiverPays is TokenInterface{
+    address owner = msg.sender;
+
+    mapping(uint256 => bool) usedNonces;
+    mapping(address => uint256) balances;
+    mapping(address => mapping(address => uint256)) allowed;
+
+    // Funds are sent at deployment time.
+    function ReceiverPays() public payable { }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balances[msg.sender] >= _value);
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        emit Transfer(msg.sender, _to, _value); //solhint-disable-line indent, no-unused-vars
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        uint256 allowance = allowed[owner][_from];
+        require(balances[_from] >= _value && allowance >= _value);
+        balances[_to] += _value;
+        balances[_from] -= _value;
+        allowed[owner][_from] -= _value;
+        emit Transfer(_from, _to, _value); //solhint-disable-line indent, no-unused-vars
+        return true;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
+    
+    function tokenBalanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value); //solhint-disable-line indent, no-unused-vars
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
+    
+    function getTokenBalance(address _contract) public view returns (uint bal){
+        return TokenInterface(_contract).balanceOf(address(this)); 
+    } 
+
+    function transferTo(address _token, uint _amount) public {
+        require(msg.sender == owner);
+        TokenInterface(_token).transfer(msg.sender, _amount);
+    }
+
+    function claimPayment(address contract_address, uint256 amount, uint256 nonce, bytes32 msgHash, uint8 v, bytes32 r, bytes32 s) public {
+        require(!usedNonces[nonce]);
+        usedNonces[nonce] = true;
+        require(ecrecover(msgHash, v, r, s) == owner, "Unauthorize transaction!");
+        TokenInterface(contract_address).transfer(msg.sender, amount);
+    }
+
+    // Destroy contract and reclaim leftover funds.
+    function kill() public {
+        require(msg.sender == owner);
+        selfdestruct(msg.sender);
+    }
+
+    function recoverAddr(bytes32 msgHash, uint8 v, bytes32 r, bytes32 s) returns (address) {
+        return ecrecover(msgHash, v, r, s);
+    }
+}
