84939d6af1c3b7cd413a644318d624327b218624
minion <minion>
2022-09-03 17:25:06 +0000

    with 💕

---
+++ contracts/mainnet/TG/TGktWy6iaMCD594SrkzY17G8S2mgx1Z8Af_Defi.sol
@@ -0,0 +1,114 @@
+//SourceUnit: defi(1)(1).sol
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+interface IERC20 {
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `to`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address to, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `from` to `to` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+}
+
+contract Defi {
+    IERC20 public USDT;
+    bool public open=true;
+    struct Info{
+        address addr;
+        uint256 idoCount;
+    }
+    mapping(string=>Info) public infos;
+    mapping(uint256=>string)public indexAddr;
+    uint256 public infoCount=0;
+    address manager;
+    address admin;
+    constructor(address _usdt){
+        USDT = IERC20(_usdt);
+        admin=msg.sender;
+        manager=msg.sender;
+    }
+    
+    function ido(uint256 count,string memory bianAddr)public{
+        require(open,"IDO CLOSED");
+        USDT.transferFrom(msg.sender,admin,count);
+        Info memory p;
+        p.addr=msg.sender;
+        p.idoCount=count;
+        infos[bianAddr]=p;
+        infoCount+=1;
+        indexAddr[infoCount]=bianAddr;
+    }
+    
+    function setAdmin(address _addr)public{
+        require(manager==msg.sender,"not alow");
+        admin=_addr;
+    }
+}
