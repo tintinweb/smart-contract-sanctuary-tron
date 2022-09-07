5360be4b75ada852420c88d12759ba4dc2481895
minion <minion>
2022-09-07 23:57:16 +0000

    with 💕

---
+++ contracts/mainnet/TP/TPKZb2PsY6hsmVXjeLtinGPLrC1QFFB4GT_BTTC_BlackHole.sol
@@ -0,0 +1,22 @@
+//SourceUnit: BTTC_BlackHole.sol
+
+ pragma solidity ^0.5.17;
+
+contract BTTC_BlackHole {
+
+     //**Burning Smart Contract for BitTorrent Chain(BTTC) only.🕳️​ Contributor: Jensy Antonio Rodriguez**
+     
+     
+     //👇​👇​**Warning: Read this before Sending your tokens**👇​👇​
+
+    //🔒BTTC or any others Tokens sent to this Smart Contract Address will be block forever!🔒
+    //Before sending your BTTC Token to this Smart Contract Address, be sure that you understand what 
+    //a blackhole is, the purpose of this and that you won´t be able to withdraw them anymore if you send any!
+    //Sending BTTC to this Smart Contract do not decrease the total supply, but will decrease the circulating
+    //supply and due to that; 📈the price will increase gradually​📈​
+
+    //**💪A strong and active community makes a difference💪**
+    
+    function() payable external {
+    }
+}
